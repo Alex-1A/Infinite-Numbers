e67fdddd0fd036f3667ab5e75f339499acd07115
Alex-1A <96305314+Alex-1A@users.noreply.github.com>
2022-08-01 18:35:45 -0400

    Fixed license typo, file now encoded in UTF-8

--- Infinite Integer.h
+++ Infinite Integer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /***********************************************************************************************************************
-* Copyright © 2022 Alex - 1A
+* Copyright Â© 2022 Alex-1A
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
